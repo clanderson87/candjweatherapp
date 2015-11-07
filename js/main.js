eb6f0ff71eca9ea3fdaadbfbf521b6e356c5ce70
Chris <cl.anderson8@gmail.com>
2015-11-07 13:26:19 -0600

    successful api response

--- js/main.js
+++ js/main.js
@@ -28,11 +28,12 @@ require(
 
   // });
 
-  var zip = $('#zip').val();
   var getWeather = function(zip){
+      zip = $('#zip').val();
+      console.log(zip);
       $.ajax({
-          url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zip +",us&units=imperial&APPID=5194556d508058f5c7a03fec4d5b05f0"
-          }).done(function(weatherData) {
+          url: "http://api.openweathermap.org/data/2.5/weather?zip="+ zip +",us&units=imperial&APPID=5194556d508058f5c7a03fec4d5b05f0"
+          }).success(function(weatherData) {
             // Execute the callback function that was sent to me
                 console.log(zip, weatherData);
           })
